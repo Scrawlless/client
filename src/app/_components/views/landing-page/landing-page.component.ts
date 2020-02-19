bbe97f37942edef4f305922d92ef9634589f636f
Dzmitry The Cat <dzmitry.kuzmitch@gmail.com>
2020-02-18 22:03:43 -0500

    [C][0006] Added form and necessary functionality

--- src/app/_components/views/landing-page/landing-page.component.ts
+++ src/app/_components/views/landing-page/landing-page.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { ApiService } from '../../../_services/api/api.service';
+import { AuthService } from '../../../_services/auth/auth.service';
 import { DataService } from "../../../_services/data/data.service";
+import { LanguageService } from '../../../_services/language/language.service';
+
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'landing-page',
@@ -14,27 +20,100 @@ export class LandingPageComponent implements OnInit {
 
   constructor(
     private api: ApiService,
+    private auth: AuthService,
     private appData: DataService,
-    private notification: MatSnackBar
+    private router: Router,
+    private notification: MatSnackBar,
+    public lang: LanguageService
   ) { }
 
+  mode: string = "register";
+  user: any;
+  authError: string = "kek";
   loading: boolean = false;
 
-  openSnackBar(message: string, action: string) {
-    this.notification.open(message, action, {
-      duration: 2000,
-    });
-  }
+  auth_form = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4)
+    ]),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4)
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4)
+    ])
+  });
 
   ngOnInit(): void {
-    this.api.test("Landing").subscribe((result: any) => {
-      this.openSnackBar(result.message, "Nice 👌");
-      this.appData.changeMessage(result.message);
-      this.loading = false;
-    }, (err) => {
-      this.openSnackBar(err.error.message, "Not Good 👎");
-      this.loading = false;
+    this.switchMode("login");
+  }
+
+  openSnackBar(message: string, action: string = "Close") {
+    this.notification.open(message, action, {
+      duration: 2000
     });
   }
 
+  switchMode(mode): void {
+    this.mode = mode;
+
+    switch (this.mode) {
+      case "login": {
+        this.auth_form.controls["name"].disable();
+        break;
+      }
+      case "register": {
+        this.auth_form.controls["name"].enable();
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+
+  onSubmit(): void {
+    if (this.auth_form.status != "INVALID") {
+      this.loading = true;
+
+      switch (this.mode) {
+        case "login": {
+          this.auth.login(this.auth_form.value).subscribe((result) => {
+            this.logIn();
+          }, (err) => {
+            this.loading = false;
+            this.openSnackBar(err.error.message);
+          });
+          break;
+        }
+        case "register": {
+          this.auth.register(this.auth_form.value).subscribe((result) => {
+            this.logIn();
+          }, (err) => {
+            this.loading = false;
+            this.openSnackBar(err.error.message);
+          });
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    }
+  }
+
+  logIn(): void {
+    this.auth.profile().subscribe(() => {
+      this.loading = false;
+      this.router.navigate(["/"]);
+    }, (err) => {
+      console.log(err);
+      this.loading = false;
+      this.auth.logout();
+      this.openSnackBar(err.error.message);
+    });
+  }
 }
