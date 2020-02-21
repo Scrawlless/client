fda4e7586923677a1ed1f872822a5293a319a2f3
DZMITRY KUZMITCH <kuzya19989@outlook.com>
2020-02-21 14:26:49 -0500

    Added new pollyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,6 +18,9 @@
  * BROWSER POLYFILLS
  */
 
+/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+import "core-js";
+
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js';  // Run `npm install --save classlist.js`.
 
@@ -53,11 +56,25 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
  */
 
 /***************************************************************************************************
- * Zone JS is required by default for Angular itself.
+ * Zone JS is required by Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
+
+import 'web-animations-js/web-animations.min';
+
 
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+/**
+ * Date, currency, decimal and percent pipes.
+ * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
+ */
+import 'intl';  // Run `npm install --save intl`.
+/**
+ * Need to import at least one locale-data with intl.
+ */
+import 'intl/locale-data/jsonp/en';
+import 'intl/locale-data/jsonp/ru';
